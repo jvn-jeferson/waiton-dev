ee7ba02b6ba9ab5c90dcee16d54aaf00fdc68036
Jhenrie Bargola <jhenrie@arbcalls.com>
2022-01-05 15:14:36 +0800

    Development Task

--- resources/views/host/individual-clients/financial-history.blade.php
+++ resources/views/host/individual-clients/financial-history.blade.php
@@ -1,78 +1,86 @@
 @extends('layouts.host-individual')
 
 @section('content')
-<div class="content-wrapper">
-    <section class="content">
-        <div class="row">
-            <div class="col-12">
-                <div class="card card-primary card-outline">
-                    <div class="card-header">
-                        <h3 class="card-title text-dark text-bold">
-                            過去の決算
-                        </h3>
-                        <a class="btn btn-primary col-2 float-right" href="{{route('create-video', ['client_id' => $hashids->encode($client->id)])}}">
-                            新規登録
-                        </a>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-hover table-striped text-center">
-                                <thead class="thead-dark">
-                                    <th>種類</th>
-                                    <th>決算日</th>
-                                    <th>提出済み申告書一式</th>
-                                    <th>承認日 • 提出日</th>
-                                    <th>説明動画</th>
-                                </thead>
-                                <tbody>
-                                    @forelse($archives as $archive)
-                                        <tr>
-                                            <td>
-                                                {{$archive->kinds ?? "" }} <br>
-                                                <a class="btn btn-primary" type="button" data-toggle="tooltip" data-placement="top" title="ACCESS FILE" href="{{route('access-data-financial-record', ['record_id' => $hashids->encodeHex($archive->id), 'client_id' => $hashids->encode($client->id)])}}">アクセス</a>
-                                            </td>
-                                            <td>@if($archive)
-                                            {{$archive->settlement_date->format('Y年m月d日')}}
-                                                @endif
-                                            </td>
-                                            <td class="text-info">
-                                                @if($archive)
-                                                    {{$archive->file->name}}
-                                                @endif
-                                            </td>
-                                            <td>
-                                                @if($archive)
-                                                {{$archive->recognition_date->format('Y年m月d日')}} • {{$archive->proposal_date->format('Y年m月d日')}}
-                                                @endif
-                                            </td>
-                                            <td class="align-items-center text-center justify-content-center">
-                                                <video style="width: 25%; border:2px darkgreen dashed; position: relative; display:flex">
-                                                    <source src="@if($archive)
-                                                        {{$archive->video_url}}
-                                                    @endif">
-                                                </video>
-                                            </td>
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td colspan="5"></td>
-                                        </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
+    <div class="content-wrapper">
+        <section class="content">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h3 class="card-title text-dark text-bold">
+                                過去の決算
+                            </h3>
+                            <a class="btn btn-primary col-2 float-right"
+                                href="{{ route('create-video', ['client_id' => $hashids->encode($client->id)]) }}">
+                                新規登録
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover table-striped text-center">
+                                    <thead class="thead-dark">
+                                        <th>種類</th>
+                                        <th>決算日</th>
+                                        <th>提出済み申告書一式</th>
+                                        <th>承認日 • 提出日</th>
+                                        <th>説明動画</th>
+                                    </thead>
+                                    <tbody>
+                                        @forelse($archives as $archive)
+                                            <tr>
+                                                <td>
+                                                    {{ $archive->kinds ?? '' }} <br>
+                                                    <a class="btn btn-primary" type="button" data-toggle="tooltip"
+                                                        data-placement="top" title="ACCESS FILE"
+                                                        href="{{ route('access-data-financial-record', ['record_id' => $hashids->encodeHex($archive->id), 'client_id' => $hashids->encode($client->id)]) }}">アクセス</a>
+                                                </td>
+                                                <td>
+                                                    @if ($archive)
+                                                        {{ $archive->settlement_date->format('Y年m月d日') }}
+                                                    @endif
+                                                </td>
+                                                <td class="text-info">
+                                                    @if ($archive)
+                                                        {{ $archive->file->name }}
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    @if ($archive)
+                                                        {{ $archive->recognition_date->format('Y年m月d日') }} •
+                                                        {{ $archive->proposal_date->format('Y年m月d日') }}
+                                                    @endif
+                                                </td>
+                                                <td class="align-items-center text-center justify-content-center">
+                                                    <center>
+                                                        <video
+                                                            style="width: 50%; border:2px darkgreen dashed; position: relative; display:flex">
+                                                            <source src=" @if ($archive)
+                                                            {{ $archive->video_url }}
+                                                            @endif">
+                                                        </video>
+                                                    </center>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="5"></td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
 
 @section('extra-scripts')
-<script>
-    $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-</script>
+    <script>
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
 @endsection
--- resources/views/host/individual-clients/video-list.blade.php
+++ resources/views/host/individual-clients/video-list.blade.php
@@ -24,26 +24,32 @@
                                     @forelse($videos as $video)
                                         <tr class="text-center">
                                             <td><input type="checkbox" name="" id=""></td>
-                                            <td>{{$video->name ?? ''}}</td>
+                                            <td>{{ $video->name ?? '' }}</td>
                                             <td>
-                                                <video style="width: 70%; border:2px darkgreen dashed; position: relative; display:flex">
-                                                    <source src="{{$video->video_url}}">
-                                                </video>
+                                                <center>
+                                                    <video
+                                                        style="width: 70%; border:2px darkgreen dashed; position: relative; display:flex">
+                                                        <source src="{{ $video->video_url }}">
+                                                    </video>
+                                                </center>
                                             </td>
-                                            <td>{{$video->created_at->format('Y-m-d')}}</td>
+                                            <td>{{ $video->created_at->format('Y-m-d') }}</td>
                                             <td>
                                                 <div class="container-fluid">
                                                     <div class="row">
                                                         <div class="col-12">
                                                             <p class="text-info text-underline" id="tocopy">
-                                                                {{$video->video_url}}
+                                                                {{ $video->video_url }}
                                                             </p>
                                                         </div>
                                                     </div>
                                                     <div class="row align-items-center justify-content-center">
-                                                        <button class="btn btn-warning col-2 mx-2" onclick="copyVSOURCE('{{$video->video_url}}')">COPY URL</button>
+                                                        <button class="btn btn-warning col-2 mx-2"
+                                                            onclick="copyVSOURCE('{{ $video->video_url }}')">COPY
+                                                            URL</button>
                                                         <div class="col-2"></div>
-                                                        <button class="btn btn-warning col-2 mx-2" type="button" onclick="changeVSOURCE('{{$video->video_url}}')">PREVIEW</button>
+                                                        <button class="btn btn-warning col-2 mx-2" type="button"
+                                                            onclick="changeVSOURCE('{{ $video->video_url }}')">PREVIEW</button>
                                                         <div class="col-2"> </div>
                                                         <button class="btn btn-danger col-2 mx-2">DELETE</button>
                                                     </div>
@@ -66,57 +72,55 @@
 
 
 
-<div class="modal fade" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Video PREVIEW</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <video src="" id="player" class="w-100" style="border: 1px solid #c6c6c6" controls >
-        </video>
-      </div>
+    <div class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Video PREVIEW</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <video src="" id="player" class="w-100" style="border: 1px solid #c6c6c6" controls>
+                    </video>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 @endsection
 
 @section('extra-scripts')
 
-<script>
-    function changeVSOURCE(srcVideo){
-        var video = document.getElementById('player');
-        video.setAttribute('src', srcVideo);
-        video.load();
-        video.play();
-        $(".modal").modal('show');
-   }
-   function copyVSOURCE(data) {
-    var dummy = document.createElement("input");
+    <script>
+        function changeVSOURCE(srcVideo) {
+            var video = document.getElementById('player');
+            video.setAttribute('src', srcVideo);
+            video.load();
+            video.play();
+            $(".modal").modal('show');
+        }
 
-    // Add it to the document
-    document.body.appendChild(dummy);
+        function copyVSOURCE(data) {
+            var dummy = document.createElement("input");
 
-    // Set its ID
-    dummy.setAttribute("id", "dummy_id");
+            // Add it to the document
+            document.body.appendChild(dummy);
 
-    // Output the array into it
-    document.getElementById("dummy_id").value=data;
+            // Set its ID
+            dummy.setAttribute("id", "dummy_id");
 
-    // Select it
-    dummy.select();
+            // Output the array into it
+            document.getElementById("dummy_id").value = data;
 
-    // Copy its contents
-    document.execCommand("copy");
+            // Select it
+            dummy.select();
 
-    // Remove it as its not needed anymore
-    document.body.removeChild(dummy);
-   }
+            // Copy its contents
+            document.execCommand("copy");
 
-
-
-</script>
+            // Remove it as its not needed anymore
+            document.body.removeChild(dummy);
+        }
+    </script>
 @endsection
