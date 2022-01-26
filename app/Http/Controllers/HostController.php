1b8521576753a13b57adbd339b350c572b91cdde
JeffBu <jbonayon15@gmail.com>
2022-01-26 09:02:58 +0800

    minor change

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -270,8 +270,6 @@ class HostController extends Controller
             'email' => 'required|email:rfc,dns|unique:users,email',
             'tax_filing_month' => 'required',
             'manager_name' => 'required',
-            'user1_email' => 'email:rfc,dns|unique:users,email',
-            'user2_email' => 'email:rfc,dns|unique:users,email',
             'manager_email' => 'required',
         ]);
 
