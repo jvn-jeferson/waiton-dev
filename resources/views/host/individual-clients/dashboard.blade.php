f46d63d8e34d0511713f4c9253e35aa042ed1b4d
jenry23 <jenrybargola7@gmail.com>
2022-02-17 08:11:58 +0800

    update 2-17-22

--- resources/views/host/individual-clients/dashboard.blade.php
+++ resources/views/host/individual-clients/dashboard.blade.php
@@ -58,7 +58,7 @@
                                         @forelse($uploads as $upload)
                                             <tr>
                                                 <td>{{date_format($upload->created_at, 'Y年m月d日')}}</td>
-                                                <td class="text-primary"><a href="{{url(Storage::url($upload->file->path))}}" download="{{$upload->file->name}}">{{$upload->file->name}}</a></td>
+                                                <td class="text-primary"><a href="{{url(Storage::disk('gcs')->url($upload->file->path))}}" download="{{$upload->file->name}}">{{$upload->file->name}}</a></td>
                                                 <td>をアップロードしました。</td>
                                                 <td>{{$upload->created_at->modify('+1 month')->format('Y年m月d日')}}</td>
                                             </tr>
@@ -98,7 +98,7 @@
                                         @forelse($downloads as $download)
                                            <tr>
                                                <td>{{$download->created_at->format('Y年m月d日')}}</td>
-                                               <td class="text-info">{{$download->file->name ?? ''}}</td>
+                                               <td class="text-info">@if($download->file) <a href="{{Storage::disk('gcs')->url($download->file->path)}}" download="{{$download->file->name}}">{{$download->file->name ?? ''}}@endif</a></td>
                                                <td>
                                                    @switch($download->status)
                                                         @case(1)
