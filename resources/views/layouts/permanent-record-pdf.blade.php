f2c49e118e6f6b8c458f8f8eb0940be82a01c961
JeffBu <jbonayon15@gmail.com>
2022-03-24 07:17:07 +0800

    setdefaultfont when fail

--- resources/views/layouts/permanent-record-pdf.blade.php
+++ resources/views/layouts/permanent-record-pdf.blade.php
@@ -9,7 +9,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container-fluid p-2">
@@ -139,5 +139,5 @@
         </div>
     </div>
 </body>
-<script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </html>
