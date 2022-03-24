01977e220229f402c9f2854a25c97388af718e73
jenry23 <jenrybargola7@gmail.com>
2022-03-24 11:25:43 +0800

    fresh

--- database/migrations/2022_03_03_123148_create_permanent_records_table.php
+++ database/migrations/2022_03_03_123148_create_permanent_records_table.php
@@ -21,12 +21,12 @@ class CreatePermanentRecordsTable extends Migration
             $table->bigInteger('pdf_file_id')->nullable();
             $table->timestamp('request_sent_at')->nullable();
             $table->bigInteger('request_sent_by_staff_id')->nullable();
-            $table->tinyInteger('has_video');
-            $table->tinyInteger('with_approval');
+            $table->string('has_video')->nullable();
+            $table->tinyInteger('with_approval')->nullable();
             $table->text('comments');
             $table->bigInteger('viewed_by_staff_id');
             $table->timestamp('response_completed_at')->nullable();
-            $table->tinyInteger('is_approved');
+            $table->tinyInteger('is_approved')->nullable();
             $table->timestamp('viewing_date')->nullable();
             $table->timestamps();
             $table->softDeletes();
