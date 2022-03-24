bd80ba7bde8e3344eead908834fc56facc38766c
jenry23 <jenrybargola7@gmail.com>
2022-03-24 17:04:42 +0800

    snappy

--- resources/views/layouts/permanent-record-pdf.blade.php
+++ resources/views/layouts/permanent-record-pdf.blade.php
@@ -9,7 +9,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.usebootstrap.com/bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <div class="container-fluid p-2">
