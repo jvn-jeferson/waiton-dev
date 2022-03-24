e2acf09c9882b36fba798de5d6e93b7f14d1ac56
jenry23 <jenrybargola7@gmail.com>
2022-03-24 17:08:51 +0800

    bootstrap for pdf

--- resources/views/layouts/permanent-record-pdf.blade.php
+++ resources/views/layouts/permanent-record-pdf.blade.php
@@ -9,7 +9,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container-fluid p-2">
